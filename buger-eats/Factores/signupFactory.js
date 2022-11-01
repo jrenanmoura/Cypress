bff98002fa2445c69b896b5af43e9901be121947
Renan <106356679+jrenanmoura@users.noreply.github.com>
2022-10-31 21:27:04 -0300

    usando o faker e gerador de cpf

--- buger-eats/Factores/signupFactory.js
+++ buger-eats/Factores/signupFactory.js
@@ -1,11 +1,19 @@
+var faker = require('faker')
+var cpf = require('gerador-validador-cpf'
+
+
+
 export default{
 
     deliver: function(){
 
+        var firstName = faker.name.firstName()
+        var lastName = faker.name.lastName()
+
         var data = {
-            name:'Fernando Papito',
-            cpf: '00000014141',
-            email: 'papito@hotmail.com',
+            name:`${firstName} ${lastName}`,
+            cpf: cpf.generate(),
+            email: faker.internet.email(firstName),
             whatsapp: '11999999999',
             endereco: {
                 cep: '04534011',
